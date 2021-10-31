dc8a69618ff6408dcc8baad440264a3dada3bea7
wilyanto <wilyanto@code.id>
2021-10-31 23:36:06 +0700

    initial commit

---
+++ lib/triangle_app.dart
@@ -0,0 +1,5 @@
+detectTriangle(num sideA, num sideB, num sideC) {
+  if (sideA < 1) {
+    throw Exception();
+  }
+}
---
+++ test/triangle_app_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:triangle_app/triangle_app.dart';
+
+void main() {
+  group('Detect the triangle', () {
+    test('Should throw Error when there is side less than 1', () {
+      expect(() => detectTriangle(-1, 2, 2), throwsA(isA<Exception>()));
+    });
+  });
+}
