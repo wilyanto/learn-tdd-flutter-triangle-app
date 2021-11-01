957f84b45034dcf4586e336563fe9a59cb0d5542
wilyanto <wilyanto@code.id>
2021-11-01 20:18:13 +0700

    validate equilateral, isosceles, and any triangle

--- lib/triangle_app.dart
+++ lib/triangle_app.dart
@@ -1,9 +1,20 @@
-detectTriangle(num sideA, num sideB, num sideC) {
+String detectTriangle(num sideA, num sideB, num sideC) {
   final sides = [sideA, sideB, sideC];
+  sides.sort();
 
   for (final side in sides) {
     if (side < 1) {
       throw Exception();
     }
   }
+
+  if (sides[0] == sides[1] && sides[0] == sides[2]) {
+    return "Segitiga Sama Sisi";
+  }
+
+  if (sides[0] == sides[1] || sides[1] == sides[2]) {
+    return "Segitiga Sama Kaki";
+  }
+
+  return "Segitiga Sembarang";
 }
--- test/triangle_app_test.dart
+++ test/triangle_app_test.dart
@@ -8,5 +8,20 @@ void main() {
       expect(() => detectTriangle(1, -2, 2), throwsA(isA<Exception>()));
       expect(() => detectTriangle(1, 2, -2), throwsA(isA<Exception>()));
     });
+
+    test('Should return "Segitiga Sama Sisi" when all sides are equal', () {
+      expect(detectTriangle(1, 1, 1), "Segitiga Sama Sisi");
+      expect(detectTriangle(1, 1, 2), isNot("Segitiga Sama Sisi"));
+    });
+
+    test('Should return "Segitiga Sama Kaki" when two sides are equal', () {
+      expect(detectTriangle(1, 1, 2), "Segitiga Sama Kaki");
+      expect(detectTriangle(1, 2, 1), "Segitiga Sama Kaki");
+      expect(detectTriangle(1, 2, 2), "Segitiga Sama Kaki");
+    });
+
+    test('Should return "Segitiga Sembarang" when no sides are equal', () {
+      expect(detectTriangle(1, 2, 3), "Segitiga Sembarang");
+    });
   });
 }
